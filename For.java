905c78a1ef07cc520ca02e05fc15c832ebc2bdd0
Dothuan6 <thuann6222@gmail.com>
2023-09-28 18:14:22 +0700

    Buoi1

---
+++ For.java
@@ -0,0 +1,9 @@
+package buoi1_bai1;
+
+public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
